2aef8eae05a2bb60a5801349573f207d5f6c45e7
lihuosheng-top <1061525487@qq.com>
2018-09-14 18:43:00 +0800

    laqu

---
+++ runtime/cache/15/a96213c16ef7cae89dd24aa035f9ef.php
@@ -0,0 +1,3 @@
+<?php
+//000000000000a:3:{i:0;a:4:{s:2:"id";i:1;s:3:"pid";i:0;s:5:"title";s:6:"首页";s:9:"url_value";s:17:"admin/index/index";}i:1;a:4:{s:2:"id";i:217;s:3:"pid";i:1;s:5:"title";s:12:"交易管理";s:9:"url_value";s:0:"";}i:2;a:4:{s:2:"id";i:243;s:3:"pid";i:217;s:5:"title";s:55:"自动任务-收盘时亏损额大于递延自动平仓";s:9:"url_value";s:18:"admin/jobs/nodelay";}}
+?>
